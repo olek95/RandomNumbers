43874532dcd405b7fc2594449262171668699741
AleksanderSklorz <olek1995s@poczta.onet.pl>
2017-01-12 21:51:00 +0100

    Update RandomNumbers.java

--- RandomNumbers.java
+++ RandomNumbers.java
@@ -2,8 +2,15 @@ package randomnumbers;
 
 import java.util.Arrays;
 import java.util.Random;
-
+/**
+ * Klasa <code>RandomNumbers</code> reprezentuje sortowanie 10000000 losowych liczb. 
+ * @author AleksanderSklorz 
+ */
 public class RandomNumbers {
+    /**
+     * Losuje 10000000 liczb. 
+     * @return tablica 10000000 losowych liczb.
+     */
     public static int[] getRandomNumbers(){
         Random rand = new Random(); 
         int[] numbers = new int[10000000];
@@ -15,12 +22,15 @@ public class RandomNumbers {
         long before = System.currentTimeMillis();
         int[] numbers = getRandomNumbers();
         System.out.println("Milisekundy losowania: " + (System.currentTimeMillis() - before));
-        //System.out.println("Liczby przed sortowaniem: ");
-        //for(int i = 0; i < numbers.length; i++) System.out.println(numbers[i]);
+        System.out.println("Liczby przed sortowaniem: ");
+        for(int i = 0; i < numbers.length; i++) System.out.print(numbers[i] + " ");
+        System.out.println();
         before = System.currentTimeMillis();
         Arrays.sort(numbers);
-        System.out.println("Milisekundy sortowania: " + (System.currentTimeMillis() - before));
-        //System.out.println("Liczby po sortowaniu: ");
-        //for(int i = 0; i < numbers.length; i++) System.out.println(numbers[i]);
+        long after = System.currentTimeMillis();
+        System.out.println("Liczby po sortowaniu: ");
+        for(int i = 0; i < numbers.length; i++) System.out.print(numbers[i] + " ");
+        System.out.println();
+        System.out.println("Milisekundy sortowania: " + (after - before));
     }
 }
